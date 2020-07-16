dcb566b2c20277ad7c2c01b79c728a6be8804f66
Yury Dmitriev <anticmex2@gmail.com>
2020-07-16 19:35:56 +0300

    Рефакторинг с учетом переработки программы в использование классов

--- stackoverflow_api.py
+++ stackoverflow_api.py
@@ -3,25 +3,55 @@ from pprint import pprint
 from datetime import datetime
 import time
 
-# создадим время в секундах для формата UNIX_TIME
-AMOUNT_DAYS_AGO_IN_SECONDS = int(time.time())-int(input("За сколько дней вывести сведения? "))*60*60*24
 
-# print(datetime.fromtimestamp(AMOUNT_DAYS_AGO_IN_SECONDS))
+class GetPythonFromStackoverflow:
+    # специализированный класс для получения информации с сайта Stackoverflow по тэгу "Python"
+
+    def __init__(self, section, tag):
+        self.section = section
+        self.tag = tag
+        # создадим время в секундах для формата UNIX_TIME
+        self.AMOUNT_DAYS_AGO_IN_SECONDS = int(time.time()) - int(
+            input("За сколько дней вывести сведения? ")) * 60 * 60 * 24
+        self.BASE_API_URL = 'https://api.stackexchange.com/2.2'
+        self.get_url = self.BASE_API_URL + section
+
+        params = self.params_get(self.BASE_API_URL, self.section, self.tag)
+        base_request = requests.get(self.get_url, params=params)
+
+        # возможно следующий блок можно вынести в функцию.. пока не определился
+        for question in base_request.json()['items']:
+            if question['creation_date'] > self.AMOUNT_DAYS_AGO_IN_SECONDS:
+                print(datetime.fromtimestamp(question['creation_date']))
+                pprint(question['title'])
+                print()
+
+    def params_get(self, url, section, tag):
+
+        if section == '/questions':
+            params_dict = {'fromdate': self.AMOUNT_DAYS_AGO_IN_SECONDS,
+                           'tagged': tag, 'site': 'stackoverflow', 'sort': 'creation'}
+        elif section == '/tags':
+            # nonlocal get_url
+            self.get_url = f'{url}{section}{tag}/faq'
+            params_dict = {'site': 'stackoverflow'}
+        return params_dict
 
 
-URL = 'https://api.stackexchange.com/2.2'
-# dop_url = '/tags/' + input("Введите поле поиска: ") + '/faq'
-dop_url = '/tags/python/faq'
-params = {'site': 'stackoverflow', 'limit': 10}
-auth = {'Authorization': 'hzUr3MIMzLQcl2R*Li*CQA))'}
+def main():
+    # Основный цикли программы
 
-# req = requests.get('https://api.stackexchange.com/2.2/tags/python/faq?site=stackoverflow', headers=auth)
-req = requests.get(URL+dop_url, params=params)
-req2 = requests.get(URL+'/questions', params={'fromdate': AMOUNT_DAYS_AGO_IN_SECONDS, 'tagged': 'python', 'site': 'stackoverflow'})
-print(req2.json())
+    print("Выберете один из разделов который хотите просмотреть:")
+    question = input("1 - вопросы, 2 - упоминания по тэгам\n")
+    if question == '1':
+        GetPythonFromStackoverflow('/questions', '/python')
+    elif question == '2':
+        print("Уточнение: за последнии 1000 дней было всего 2 упоминания.")
+        print("Используйте эту информацию при выборе количества дней.")
+        GetPythonFromStackoverflow('/tags', '/python')
+    else:
+        print("Вы не выбрали один из двух вариантов. Программа будет закрыта.")
+        print("Спасибо за использование данной программы.")
 
-for i in req2.json()['items']:
-    if i['creation_date'] > AMOUNT_DAYS_AGO_IN_SECONDS:
-        print(datetime.fromtimestamp(i['creation_date']))
-        pprint(i['title'])
-        print()
+
+main()
